8aedb91cc82ed8ac9057dc0066530a61e875e286
thibault <derbomez.thibault@gmail.com>
2020-11-02 16:22:28 +0100

    Switch Ok

--- src/app/switch/switch.component.ts
+++ src/app/switch/switch.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switch',
   templateUrl: './switch.component.html',
   styleUrls: ['./switch.component.css']
 })
-export class SwitchComponent implements OnInit {
+export class SwitchComponent {
+
+  status:boolean = true ;
+  textStatus:string = "ON"
+  backgroundColor:string = "#66cf91" ;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  changeStatus() {
+    this.status = !this.status ;
 
+    if(this.status){
+      
+      this.backgroundColor = "#66cf91" ;
+      this.textStatus = "ON" ;
+      console.log("valeur 1");
+    }
+    else{
+      this.backgroundColor = "#D3D3D3" ;
+      this.textStatus = "OFF" ;
+      console.log("valeur 0");
+    }
+  }
 }
